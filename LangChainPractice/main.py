77f3374ec102c81d0cd5607139c5cb997bb208ce
Felipe Martinez <104150935+darthfelipe21@users.noreply.github.com>
2025-09-26 17:11:40 -0300

    Delete LangChainPractice directory

--- LangChainPractice/main.py
+++
@@ -1,9 +0,0 @@
-from app import chat_with_memory
-
-def main():
-    print(chat_with_memory("Hola, me llamo SOYUNAPRUEBA"))
-    print(chat_with_memory("Recuerdas mi nombre??"))
-    print(chat_with_memory("Dime cual es la mejor cancion de rock de todos los tiempos?"))
-
-if __name__ == "__main__":
-    main()
